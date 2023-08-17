0c7eb93b7ee5381dcb50ae89237ae15ddca0c975
lg <longislanduss@outlook.com>
2023-08-17 21:02:17 +0000

    upgrade

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -4,7 +4,7 @@ var UnminedMapProperties = {
     maxZoom: 2,
     defaultZoom: 0,
     imageFormat: "webp",
-    minRegionX: -127,
+    minRegionX: -133,
     minRegionZ: -145,
     maxRegionX: 76,
     maxRegionZ: 98,
